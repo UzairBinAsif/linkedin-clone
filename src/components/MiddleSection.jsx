abc7e574ada8204619362e7cc49e011677042493
UzairBinAsif <shaikhuzairasif14@gmail.com>
2026-04-08 18:25:06 +0500

    complete middle section

--- src/components/MiddleSection.jsx
+++ src/components/MiddleSection.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import PostStart from './PostStart'
 import Post from './Post'
+import postImg1 from '../assets/postImg1.jpg'
+import postImg2 from '../assets/postImg2.jpg'
+import r11 from '../assets/r11.png'
+import r13 from '../assets/r13.png'
+import r14 from '../assets/r13.png'
 
 const MiddleSection = () => {
   return (
     <div className='mt-6 w-[49%]'>
         <PostStart />
         <hr className='text-[#DFDEDA] my-3' />
-        <Post />
+        <Post cmntImg={r11} cmntName={"Tahami Kashif"} idImg={r13} idName={"Engr. Muhammad Rizwan Razi"} idDesc={"Electrical Engineer (UIT-NED-25)| Solar System Installer | Shopify Develop."} idTime={"11h"} postImg={postImg1} postDesc={`🚨 Urgent Hiring Alert: 
+
+                Multiple Internship Roles!
+
+                Ready to turn your coding knowledge into real-world industrial impact?
+
+                • Web & Systems (Full Stack)
+                • Core Logic & APIs (Backend)
+                • Interfaces & UX (Frontend)
+
+                Comment #INTERESTED below to secure your spot now!
+
+                #Hiring #DecodeLabs #TechJobs #Interns`}/>
+        <Post cmntImg={r11} cmntName={"Junaid Sangi"} idImg={r14} idName={"Mohsin Raza"} idDesc={"Agentic AI Developer | AI Engineer | DevOps Engineer | n8n Automation | "} idTime={"22h"} postImg={postImg2} postDesc={`🚀 OpenClaw Tutorial for Beginners | Setup + WhatsApp Integration`}/>
     </div>
   )
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import r11 from '../assets/r11.png'
-import r13 from '../assets/r13.png'
-import { IoMdClose } from 'react-icons/io'
+import { IoIosSend, IoMdClose } from 'react-icons/io'
 import { HiOutlineDotsHorizontal } from 'react-icons/hi'
 import { TiWorld } from 'react-icons/ti'
-import { FaPlus } from 'react-icons/fa6'
+import { FaPlus, FaRegCommentDots } from 'react-icons/fa6'
+import { BiLike, BiRepost } from 'react-icons/bi'
 
-const Post = () => {
+const Post = ({cmntImg, cmntName, idImg, idName, idDesc, idTime, postDesc, postImg}) => {
+    const seeMore = (e) => {
+        if (e.target.innerText == "less") {
+            // e.target.closest("div").innerText = e.target.closest("div").innerText.slice(0, 29)
+            e.target.innerText = "... more"
+        } else if (e.target.innerText == "... more") {
+            // e.target.closest("div").innerText = e.target.closest("div").innerText.slice(0,)
+            e.target.innerText = "less"
+        }
+    }
   return (
-    <div className='bg-white rounded-lg p-2 border border-[#DFDEDA] flex flex-col gap-3'>
+    <div className='bg-white rounded-lg p-2 border border-[#DFDEDA] flex flex-col gap-3 mb-2'>
         <div className='flex justify-between items-center'>
             <div className='flex gap-2 items-center'>
-                <img src={r11} alt="" className='w-6 h-6' />
-                <p><span className='font-semibold'>Tahami Kashif</span> commented on this</p>
+                <img src={cmntImg} alt="" className='w-6 h-6' />
+                <p><span className='font-semibold'>{cmntName}</span> commented on this</p>
             </div>
             <div className='flex gap-4 items-center'>
                 <HiOutlineDotsHorizontal />
@@ -22,12 +30,12 @@ const Post = () => {
         <hr className='text-[#DFDEDA] my-1' />
         <div className='flex justify-between'>
             <div className='flex gap-1'>
-                <img src={r13} alt="" className='w-12 h-12' />
+                <img src={idImg} alt="" className='w-12 h-12' />
                 <div>
-                    <p className='font-semibold'>Engr. Muhammad Rizwan Razi<span className='text-[12px] font-normal text-gray-500'> • 2nd</span></p>
-                    <p className='text-[12px] text-gray-500'>Electrical Engineer (UIT-NED-25)| Solar System Installer | Shopify Develop.</p>
+                    <p className='font-semibold'>{idName}<span className='text-[12px] font-normal text-gray-500'> • 2nd</span></p>
+                    <p className='text-[12px] text-gray-500'>{idDesc}</p>
                     <div className='flex items-center gap-1'>
-                        <p className='text-[12px] text-gray-500'>11h •</p>
+                        <p className='text-[12px] text-gray-500'>{idTime} •</p>
                         <TiWorld />
                     </div>
                 </div>
@@ -36,6 +44,32 @@ const Post = () => {
                 <FaPlus />
                 Follow
             </div>
+        </div>
+        <div>
+            <div className='text-[14px] text-[#222222]'>
+                {postDesc}
+                <button onClick={seeMore} className='cursor-pointer text-black font-semibold'>... more</button>
+            </div>
+            <img src={postImg} alt="" />
+        </div>
+        <hr className='text-[#DFDEDA] my-1' />
+        <div className='flex justify-center'>
+            <button className='w-[23%] flex flex-col justify-center items-center font-semibold text-[#404040] rounded cursor-pointer hover:bg-[#F3F3F3]'>
+                <BiLike />
+                <p>Like</p>
+            </button>
+            <button className='w-[23%] flex flex-col justify-center items-center font-semibold text-[#404040] rounded cursor-pointer hover:bg-[#F3F3F3]'>
+                <FaRegCommentDots />
+                <p>Comment</p>
+            </button>
+            <button className='w-[23%] flex flex-col justify-center items-center font-semibold text-[#404040] rounded cursor-pointer hover:bg-[#F3F3F3]'>
+                <BiRepost />
+                <p>Repost</p>
+            </button>
+            <button className='w-[23%] flex flex-col justify-center items-center font-semibold text-[#404040] rounded cursor-pointer hover:bg-[#F3F3F3]'>
+                <IoIosSend />
+                <p>Share</p>
+            </button>
         </div>
     </div>
   )
